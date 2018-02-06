1f6e77bc92856f36ccaf3fdc8554e4ce8467bdff
Tintu C Raju <tintumon18@gmail.com>
2018-02-06 09:23:18 +0530

    Update malluscript.js

--- malluscript.js
+++ malluscript.js
@@ -2,9 +2,17 @@
      @author : Tintu C Raju
 */
 
-MalluScript = function(){
+MalluScript = function(options={}){
 
+	this.version  = "0.1";
+	this.getVersion = function(){
+		return this.version;
+	}
+	
 	this.mapping = {	
+
+		"ശരിയാണ് ":"== true",
+		"തെറ്റാണ് ": "== false",
 		"പറയുക" : "alert" , 
 		"പുതിയ": "new",
 		"ഇപ്പൊ" : 'if (',
@@ -27,9 +35,53 @@ MalluScript = function(){
 		"സമം":"=",
 		"തുല്യം":"==",
 		"തെരഞ്ഞെടുക്കുക":"document.getElementById",
-		"വിവരങ്ങൾ ":"innerHTML",
-		"":""
-	  };
+		"ആവട്ടെ": "let",
+		"അവസ്ഥ": "case",
+		"അസാധു": "null",
+		"ഇറക്കുമതി": "import",
+		"ചെയ്യുക": "do",
+		"തിരിക്കുക": "switch",
+		"തിരുത്തൽ": "debugger",
+		"തുടരുക": "continue",
+		"തെറ്റ്": "false",
+		"നല്‍കുക": "yield",
+		"നിർവഹിക്കുന്നു": "implements",
+		"പരിവര്‍ത്തനം": "var",
+		"പിടിക്കുക": "catch",
+		"പുതിയ": "new",
+		"പ്രയോഗം": "function",
+		"പൊതു": "public",
+		"ഭാണ്‌ഡം": "package",
+		"മടക്കം": "return",
+		"മുടക്കുക": "break",
+		"മൂല്യനിർണ്ണയം": "eval",
+		"വാദം": "arguments",
+		"വ്യർത്ഥം": "void",
+		"വ്യാപിപ്പിക്കുന്നു": "extends",
+		"ശരി": "true",
+		"ശാശ്വതം": "const",
+		"ശ്രമിക്കുക": "try",
+		"സമ്പര്‍ക്കമുഖം": "interface",
+		"സുരക്ഷിതമാക്കപ്പെട്ട": "protected",
+		"സ്വകാര്യ": "private",
+		"സ്ഥായി": "static",
+		"അമർത്തുക":"onclick",
+		"അമർത്തുമ്പോൾ":"onclick",
+		"ഞെക്കുക":"onclick",
+		"രണ്ട് അമർത്തൽ":"dblclick",
+		"രണ്ട് ഞെക്ക്":"dblclick",
+		"കീ ഞെക്കുക്കുമ്പോൾ":"keypress",
+		"കീ എടുക്കുമ്പോൾ":"keyup",
+		"തയ്യാറാകുമ്പോൾ":"onload",
+		"ജാലകം":"window",
+		"രേഖ":"document",
+		"തുടർച്ച":"for ( var __i= ",
+		"മുതൽ":"; __i < ",
+		"വരെ":"; __i++ )",
+
+
+
+	};
 		  
 	this.exec= function(_script){
 		eval(this.compile(_script));
@@ -37,7 +89,7 @@ MalluScript = function(){
 		
 		
 	this.init = function(){
-		var scripts = document.querySelectorAll("[type='text/mangascript']");
+		var scripts = document.querySelectorAll("[type='text/malluscript']");
 		_script = scripts[0].innerHTML;
 		_script = this.compile(_script);
 		eval(_script);
@@ -59,7 +111,7 @@ MalluScript = function(){
 	}
 }
 
- document.addEventListener('DOMContentLoaded', function(){
+		document.addEventListener('DOMContentLoaded', function(){
 		var ob = new MalluScript();
 		ob.init();
 	}, false);
